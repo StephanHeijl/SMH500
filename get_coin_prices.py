b9cac2f3b14fb79e24e75a74a7946094b79bb52d
root <root@MEMEMACHINE.localdomain>
2017-10-22 16:42:13 +0200

    converted script to sqlite db

--- get_coin_prices.py
+++ get_coin_prices.py
@@ -4,9 +4,10 @@ import datetime
 import time
 import pandas
 import numpy as np
+import sqlite3
 from tqdm import tqdm
 
-url = "https://min-api.cryptocompare.com/data/pricehistorical?fsym={coin}&tsyms=USD&ts={timestamp}&extraParams=SMH500"
+url = "https://min-api.cryptocompare.com/data/pricehistorical?fsym={coin}&tsyms=USD,EUR,BTC&ts={timestamp}&extraParams=SMH500"
 
 coins = set(["BTC"])
 with open("markets.json") as f:
@@ -19,19 +20,29 @@ with open("markets.json") as f:
 today = datetime.datetime.now()
 
 timestamps = []
-days = 52
-day_no = 7
+days = 365
+day_no = 1
 delay = 2
 for i in range(days):
     d = today - datetime.timedelta(days=day_no * i)
     timestamps.append(int(time.mktime(d.timetuple())))
 
 coins = list(coins)
-#coins = ["BTC", "ETC"]
-empty = np.empty((len(coins), days))
-empty[:] = None
-data = pandas.DataFrame(empty, index=coins, columns=timestamps)
 timestamps.reverse()
+
+conn = sqlite3.connect('coins.db')
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS
+coins(
+    ts INTEGER,
+    coin CHAR(10),
+    USD REAL,
+    EUR REAL,
+    BTC REAL
+)
+""")
+
 
 for coin in tqdm(coins):
     for ts in tqdm(timestamps):
@@ -40,10 +51,14 @@ for coin in tqdm(coins):
         while not done:
             try:
                 r = requests.get(url.format(coin=coin, timestamp=ts))
-                data.loc[coin, ts] = r.json().values()[0]["USD"]
+                values = r.json().values()[0]
+                values["ts"] = int(ts)
+                values["coin"] = coin
+                cursor.execute("INSERT INTO coins VALUES({ts}, '{coin}', {USD}, {EUR}, {BTC})".format(**values))
+                conn.commit()
                 done = True
             except:
-                time.sleep(10)
+                sleep(10)
         time.sleep(delay)
 
 data.to_csv("coins.csv")
